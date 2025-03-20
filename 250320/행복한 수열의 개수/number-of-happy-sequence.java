231b02f66f3f7383f1c84b3ba5210023928a79f2
강수진 <93658676+szzing@users.noreply.github.com>
2025-03-20 17:00:22 +0900

    Title: 행복한 수열의 개수, Time: 219ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/행복한 수열의 개수/number-of-happy-sequence.java
+++ 250320/행복한 수열의 개수/number-of-happy-sequence.java
@@ -18,12 +18,18 @@ public class Main {
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            findHappyNumList(i, true);
-            findHappyNumList(i, false);
+        if(m == 1) {
+            numListCnt = 2 * n;
+
+        } else {
+            for(int i = 0; i < n; i++) {
+                findHappyNumList(i, true);
+                findHappyNumList(i, false);
+            }
         }
 
         System.out.println(numListCnt);
+        
     }
 
     public static void findHappyNumList(int x, boolean isRow) {
@@ -31,13 +37,14 @@ public class Main {
         int prev = isRow ? grid[x][0] : grid[0][x];
 
         for (int i = 1; i < n; i++) {
-            int current = isRow ? grid[x][i] : grid[i][x];
+            int curr = isRow ? grid[x][i] : grid[i][x];
 
-            if (current == prev) {
+            if (curr == prev) {
                 cnt++;
+
             } else {
                 cnt = 1;
-                prev = current;
+                prev = curr;
             }
 
             if (cnt == m) {
